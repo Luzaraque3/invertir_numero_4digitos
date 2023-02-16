964394a92e8dbf4e336fc9ed7d34b79380daf214
sistemas <wendyvargas@colegioguanenta.edu.co>
2023-02-16 15:32:27 -0500

    Se agrego diagrama de flujo

---
+++ invertir_numero_4digitos.py
@@ -0,0 +1,12 @@
+# Programa para invertir un numero de 4 digitos. Ej. Si ingresa 1234, devuelve 4321
+
+print("-------------------------------------")
+print("-------------------------------------")
+print("-------------------------------------")
+
+# input
+
+
+# processing
+
+# output
